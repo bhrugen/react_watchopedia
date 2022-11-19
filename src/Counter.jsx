788a0cfc51f8715c998f34e66f6ce4d1e745288a
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-19 15:18:19 -0600

    Gotchas with useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
 
 const Counter = () => {
-  const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(() => {
+    console.log("hello");
+    return 10;
+  });
+
+  function incrementCounter() {
+    setCounter(counter + 1);
+  }
+
+  function decrementCounter() {
+    setCounter((prevState) => {
+      return prevState - 1;
+    });
+    setCounter((prevState) => {
+      return prevState - 1;
+    });
+  }
 
   return (
     <div className="col-12 col-md-4 offset-md-4 border  text-white">
       <span className="h2 pt-4 m-2 text-white-50">Fun Counter </span>
       <br />
-      <button className="btn btn-success m-1">+1</button>
-      <button className="btn btn-danger m-1">-1</button>
+      <button className="btn btn-success m-1" onClick={incrementCounter}>
+        +1
+      </button>
+      <button className="btn btn-danger m-1" onClick={decrementCounter}>
+        -1
+      </button>
       <br />
       <span className="h4">
         Counter: &nbsp;
