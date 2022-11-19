2353596596270ce4988d0605f3338970d05298bd
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-19 15:04:17 -0600

    UseState in Action

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [counter, setCounter] = useState(10);
+
   return (
     <div className="col-12 col-md-4 offset-md-4 border  text-white">
       <span className="h2 pt-4 m-2 text-white-50">Fun Counter </span>
@@ -8,7 +12,7 @@ const Counter = () => {
       <br />
       <span className="h4">
         Counter: &nbsp;
-        <span className="text-success">0</span>
+        <span className="text-success">{counter}</span>
       </span>
     </div>
   );
