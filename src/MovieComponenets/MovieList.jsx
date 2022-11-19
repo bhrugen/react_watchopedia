4ac9b0605d12450411b24e92e917d57058643efc
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-19 15:46:16 -0600

    Assignment 12 - Display Movie List

--- src/MovieComponenets/MovieList.jsx
+++ src/MovieComponenets/MovieList.jsx
@@ -1,2 +1,10 @@
-const MovieList = () => {};
+const MovieList = (props) => {
+  return (
+    <div className="text-white">
+      {props.movieList.map((movie, index) => {
+        return <div key={index}>{movie}</div>;
+      })}
+    </div>
+  );
+};
 export default MovieList;
--- src/MovieComponenets/MoviePage.jsx
+++ src/MovieComponenets/MoviePage.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import AddMovie from "./AddMovie";
 import MovieList from "./MovieList";
 //movieList: ["Die Hard", "Harry Potter"]
 const MoviePage = () => {
+  const [movieState, setMovieState] = useState(() => {
+    return { movieList: ["Die Hard", "Harry Potter"] };
+  });
+
   return (
     <div className="container col-12 col-md-6 my-3 border">
       <AddMovie />
-      <MovieList />
+      <MovieList movieList={movieState.movieList} />
     </div>
   );
 };
